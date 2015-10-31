1feab6a92ba4966f438dccf41d1b5eab9459b7c5
Kenji Crosland <kenji.crosland@gmail.com>
2015-10-31 14:44:39 -0700

    Added image upload functionality. And it worked!

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var url = require("url")
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for" + pathname + "received.");
-
-    route(handle, pathname, response);
+    console.log("Request for " + pathname + "received.");
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
